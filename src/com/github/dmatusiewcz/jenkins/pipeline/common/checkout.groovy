229d4dd7bdc48f155d8b9e8bc5ab2b570660b4ad
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-03-26 17:54:39 +0100

    Checkout function

--- src/com/github/dmatusiewcz/jenkins/pipeline/common/checkout.groovy
+++ src/com/github/dmatusiewcz/jenkins/pipeline/common/checkout.groovy
@@ -1,5 +1,5 @@
 package com.github.dmatusiewcz.jenkins.pipeline.common
 
-def check_out_gitlab_repo(group, repo, branch, creds) {
-  checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', depth: 0, noTags: true, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: repo]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: creds, url: 'git@gitlab.motdev.org.uk:' + group + '/' + repo +'.git']]]
+def check_out_gitlab_repo(credentialsId, url) {
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CommonPipelineLibrary']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: url]]])
 }
