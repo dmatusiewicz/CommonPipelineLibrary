63237b40f3005d92e1336d48863bd18c5284e710
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-03-26 18:18:52 +0100

    Checkout function

--- src/com/github/dmatusiewcz/jenkins/pipeline/common/Pipeline.groovy
+++ src/com/github/dmatusiewcz/jenkins/pipeline/common/Pipeline.groovy
@@ -2,8 +2,10 @@ package com.github.dmatusiewcz.jenkins.pipeline.common
 
 class Pipeline implements Serializable {
   String branch
-  static def check_out_gitlab_repo(credentialsId, url) {
-    //  println credentialsId + " " + url
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CommonPipelineLibrary']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: url]]])
-  }
+
+}
+
+def check_out_gitlab_repo(credentialsId, url) {
+  //  println credentialsId + " " + url
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CommonPipelineLibrary']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: url]]])
 }
