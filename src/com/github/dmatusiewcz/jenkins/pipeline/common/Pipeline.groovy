1ec56d542a1482b3b057205921b72ffba3652f47
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-03-26 15:28:00 +0100

    Gradle and test test

--- src/com/github/dmatusiewcz/jenkins/pipeline/common/Pipeline.groovy
+++ src/com/github/dmatusiewcz/jenkins/pipeline/common/Pipeline.groovy
@@ -1,4 +1,4 @@
-package common
+package com.github.dmatusiewcz.jenkins.pipeline.common
 
 class Pipeline implements Serializable {
   String branch
---
+++ test/com/github/dmatusiewcz/jenkins/pipeline/common/PipelineTest.groovy
@@ -0,0 +1,10 @@
+import com.github.dmatusiewcz.jenkins.pipeline.common.Pipeline
+import org.junit.Test
+import static org.junit.Assert.*
+
+class PipelineTest {
+  @Test
+  public void branchTest(){
+    
+  }
+}
