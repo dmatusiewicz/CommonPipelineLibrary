66e5f7b822177754adbfe1313699f11c434d7150
Dawid Matusiewicz <d.matusiewicz@kainos.com>
2017-03-26 19:29:56 +0100

    Checkout function

--- src/com/github/dmatusiewcz/jenkins/pipeline/common/Utils.groovy
+++ src/com/github/dmatusiewcz/jenkins/pipeline/common/Utils.groovy
@@ -6,3 +6,5 @@ import jenkins.model.Jenkins.*
 def check_out_gitlab_repo(credentialsId, url) {
   checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CommonPipelineLibrary']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: url]]])
 }
+
+return this;
