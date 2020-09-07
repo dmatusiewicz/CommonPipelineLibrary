97639242e29380a82853bd70e6e92a1c8c9c15bd
Dawid Matusiewicz <dawidm@kainos.com>
2020-09-07 15:23:07 +0200

    asd

--- src/com/github/dmatusiewcz/jenkins/pipeline/common/Pipeline.groovy
+++
@@ -1,5 +0,0 @@
-package com.github.dmatusiewcz.jenkins.pipeline.common
-
-class Pipeline implements Serializable {
-  String branch
-}
--- src/com/github/dmatusiewcz/jenkins/pipeline/common/Utils.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/groovy
-package com.github.dmatusiewcz.jenkins.pipeline.common
-
-def check_out_gitlab_repo(credentialsId, url) {
-  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'CommonPipelineLibrary']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: url]]])
-}
