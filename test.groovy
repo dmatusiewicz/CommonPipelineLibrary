aee63ca0769e719bd55e78a6b35756e5d6760bd6
Dawid Matusiewicz <dawidm@kainos.com>
2020-09-07 13:46:50 +0200

    asd

---
+++ test.groovy
@@ -0,0 +1,10 @@
+
+
+def getCurl(host, port, path) {
+   returnValue = sh (
+       script: "curl -s http://" + host + ":" + port + path,
+       returnStdout: true
+       )
+   return returnValue
+}
+
